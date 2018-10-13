51adb37f375453142254e181846f54a3db7702e9
kivitea <kivi.ivan.tea@gmail.com>
2018-10-13 12:40:38 +0300

    initial commit

---
+++ lssn_7/lssn_7/Dubler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lssn_7
+{
+    class Dubler
+    {
+        public int RoundNum, CurrentValue, Answer;
+        private Stack<int> Values;
+
+        Random rnd = new Random();
+        
+        public Dubler()
+        {
+            RoundNum = 1;
+            Values = new Stack<int>();
+            CurrentValue = 1;
+
+            Answer = rnd.Next(2, 1000);
+        }
+
+        private void newRound()
+        {
+            RoundNum++;
+            Values.Push(CurrentValue);
+        }
+
+        public void Sum()
+        {
+            newRound();
+            CurrentValue++;
+        }
+
+        public void Mult()
+        {
+            newRound();
+            CurrentValue = CurrentValue*2;
+        }
+
+        public void Cancell()
+        {
+            if (Values.Count == 0) return;
+
+            CurrentValue = Values.Pop();
+            RoundNum--;
+        }
+    }
+}
