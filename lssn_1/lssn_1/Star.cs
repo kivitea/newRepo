a18cc762734a2676601ad1aa4be566773ed6110c
kivitea <kivi.ivan.tea@gmail.com>
2018-10-20 16:41:21 +0300

    initial commit

---
+++ lssn_1/lssn_1/Star.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace lssn_1
+{
+    class Star: BaseObject
+    {
+        Pen pen;
+
+        public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            chPen();
+        }
+
+        /// <summary>
+        /// Выбор цвета при создании
+        /// </summary>
+        private void chPen()
+        {
+            switch (rnd.Next(1, 5) % 5)
+            {
+                case 1: pen = Pens.Red; break;
+                case 2: pen = Pens.Yellow; break;
+                case 3: pen = Pens.Green; break;
+                case 4: pen = Pens.Blue; break;
+            }
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawLine(pen, Pos.X, Pos.Y, Pos.X + Size.Width, Pos.Y + Size.Height);
+            Game.Buffer.Graphics.DrawLine(pen, Pos.X + Size.Width, Pos.Y, Pos.X, Pos.Y + Size.Height);
+        }
+
+        /// <summary>
+        /// Перемещение объекта. При уходе за край экрана, позиция и цвет определяются заново
+        /// </summary>
+        public override void Update()
+        {
+            Pos.X -= Dir.X;
+            if (Pos.X < 0)
+            {
+                Pos.X = Game.Width + Size.Width;
+                Pos.Y = rnd.Next(1, 9) * 60;
+
+                chPen();
+            }
+        }
+    }
+}
